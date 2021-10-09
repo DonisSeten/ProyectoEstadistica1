37ba9fc702c318170cafbe98da83041ad47f96fc
DonisSeten <92006051+DonisSeten@users.noreply.github.com>
2021-10-09 17:11:35 -0600

    Add files via upload

---
+++ PROYECTO_ESTADISTICA/src/proyecto_estadistica/PROYECTO_ESTADISTICA.java
@@ -0,0 +1,13 @@
+package proyecto_estadistica;
+
+
+public class PROYECTO_ESTADISTICA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
